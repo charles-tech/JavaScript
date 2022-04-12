e9587ee763fee402b3b04aed6eaae5bc5b6b34e1
charles-tech <charles.fernando.miranda@gmail.com>
2022-04-11 22:42:25 -0300

    Map

---
+++ map.js
@@ -0,0 +1,13 @@
+
+let usuarios = new Map();
+
+usuarios.set('Luiz', 'Admin');
+usuarios.set('Charles', 'Admin');
+usuarios.set('Rafaela', 'User');
+usuarios.set('Laura', 'Admin');
+
+for(u of usuarios)
+console.log(u);
+
+
+
